86690ddaf2bc23158f2844641dd5206153a04f73
Jack Harris <jack@zipcoders-MacBook-Pro-49.local>
2019-11-20 17:06:21 -0500

    all tests pass

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,39 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+//    private Integer[] intArray;
+
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+//    public Stream<Integer> removeDuplicatesExactly (Integer[] intArray, Integer n){
+//        return Arrays.stream(intArray)
+//                .filter(num -> countDuplicates(intArray) != n);
+//    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        return Arrays.stream(array)
+                .filter(num -> countDuplicates(num) < maxNumberOfDuplications).toArray(Integer[]::new);    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        return Arrays.stream(array)
+                .filter(num -> countDuplicates(num) != exactNumberOfDuplications).toArray(Integer[]::new);
+    }
+
+    public Long countDuplicates (Integer num){
+        return Arrays.stream(array).filter(n -> n == num).count();
+    }
+
 }
